743fe6ea1e6de3cabf040b5c4085d6a9cfd70901
idk <itisdeepakkumar@outlook.com>
2022-02-24 02:20:10 +0530

    productlists

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,76 +1,80 @@
-import styled from "styled-components"
-// import Announcement from "../components/Announcement";
-// import NavBar from "../components/NavBar";
-// import Products from "../components/Products";
-// import Newsletter from "../components/Newsletter";
-// import Footer from "../components/Footer";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import NavBar from "../components/NavBar";
+import Products from "../components/Products";
+import Newsletter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 
-const Container=styled.div``;
-const Title=styled.h2`
-margin:20px;
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
 `;
-const FilterContainer=styled.div`
-display:flex;
-justify-content:space-between;
+const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
-const Filter=styled.div`
-margin:20px;
+const Filter = styled.div`
+  margin: 20px;
 `;
-const FilterText=styled.span`
-font-size:20px;
-font-weight:600;
-margin-right:20px;
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
 `;
-const Select=styled.select`
-padding:10px;
-margin-right:20px;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  // height: 35px;
 `;
-const Option=styled.option``;
+const Option = styled.option`
+  margin: 10px 0;
+`;
 
 const ProductList = () => {
   return (
     <Container>
-      {/* <NavBar />
-      <Announcement /> */}
-      <Title>Clothes</Title>
+      <Announcement />
+      <NavBar />
+      <Title>Fashion Products</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products: </FilterText>
-        </Filter>
-        <Select>
+          <Select>
             <Option disabled selected>
-                Colour
+              Color
             </Option>
             <Option>Black</Option>
             <Option>Blue</Option>
             <Option>Green</Option>
             <Option>Magenta</Option>
             <Option>Purple</Option>
-        </Select>
-        <Select>
-        <Option disabled selected>
-                Size
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
             </Option>
             <Option>S</Option>
             <Option>M</Option>
             <Option>L</Option>
             <Option>XL</Option>
             <Option>XXL</Option>
-        </Select>
+          </Select>
+        </Filter>
         <Filter>
           <FilterText>Sort Products: </FilterText>
           <Select>
-              <Option selected>Newest</Option>
-              <Option>Price (asc)</Option>
-              <Option>Price (desc)</Option>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      {/* <Products /> */}
-      {/* <Newsletter /> */}
-      {/* <Footer /> */}
+      <Products />
+      <Newsletter />
+      <Footer />
     </Container>
   );
-}
+};
 
-export default ProductList
+export default ProductList;
