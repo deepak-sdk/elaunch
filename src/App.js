f26f322e9f6eec8256a6e60020afa6a2363627b0
idk <itisdeepakkumar@outlook.com>
2022-02-24 02:19:05 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <>
-    <Home/>
+    <ProductList/>
     </>
   );
 }
